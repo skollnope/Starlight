c2bdc367c703cb6bdad73ebad397bfb212d5eb52
skollnope <antonindebonoa@outlook.fr>
2024-03-19 22:02:36 +0100

    added 1st action

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -4,24 +4,20 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import datetime
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionGetLocaltime(Action):
+    def name(delft) -> Text:
+        return "action_get_localtime"
+    
+    async def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_time = datetime.datetime.now()
+        message = current_time.strftime("%H:%M:%S")
+        dispatcher.utter_message(text=message)
+        return []
