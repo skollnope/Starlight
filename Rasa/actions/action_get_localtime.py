f48edd0eb08fa978a8035e4e1ea96fcad07dc9b2
skollnope <antonindebonoa@outlook.fr>
2024-03-22 10:26:52 +0100

    better messages

--- Rasa/actions/action_get_localtime.py
+++ Rasa/actions/action_get_localtime.py
@@ -2,6 +2,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import datetime
+from pytz import timezone
 
 class ActionGetLocaltime(Action):
     def name(delft) -> Text:
@@ -13,14 +14,15 @@ class ActionGetLocaltime(Action):
            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         current_time = datetime.datetime.now()
-        message = "It's " + current_time.strftime("%H:%M:%S")
+        message: str
+        localtime = current_time.strftime("%H:%M:%S")
 
         # Check if location entity is present in tracker
         country = next(tracker.get_latest_entity_values("country"), None)
         if country is not None:
-            message += f" in {country}"  # Use f-string for clean formatting
+            message = f"I can't give you the current time in {country} but it's {localtime} on you computer"
         else:
-            message += " on your computer"  # Or any default message
+            message = f"it's {localtime}"
 
         dispatcher.utter_message(text=message)        
         return []
