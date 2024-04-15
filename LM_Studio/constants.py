227cd836a84371eefb0e4ec11c61a3e9ddb7dd43
skollnope <antonindebonoa@outlook.fr>
2024-04-15 21:57:17 +0200

    update constants

--- LM_Studio/constants.py
+++ LM_Studio/constants.py
@@ -2,8 +2,10 @@ MODEL_IP="192.168.1.40"
 MODEL_PORT="1234"
 MODEL_URL="http://" + MODEL_IP + ":" + MODEL_PORT + "/v1"
 
-API_KEY="lm-studio"
-DEFAULT_MODEL="current-model"
+MODEL_GPT35="gpt-3.5-turbo"
+MODEL_GPT4="gpt-4"
+MODEL_GPT4_TURBO="gpt-4-turbo-preview"
 
-#SYSTEM_PROMPT="You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful."
-SYSTEM_PROMPT="Tu est un assistant français, ta langue principale est le français. tu t'appelles \"Starlight\". Tu dois tout faire pour répondre aux questions qui te sont posées."
+DEFAULT_MODEL=MODEL_GPT35
+
+SYSTEM_PROMPT="You are an virtual assistant calles \"Satrlight\". You always provide well-reasoned answers that are both correct and helpful."
