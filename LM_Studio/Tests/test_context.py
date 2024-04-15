d409e8cdb3e9b333695e30cbe20c3a55e981111f
skollnope <antonindebonoa@outlook.fr>
2024-04-15 23:47:39 +0200

    Create test_context.py

---
+++ LM_Studio/Tests/test_context.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+from Starlight.LM_Studio import constants as cst
+from Starlight.LM_Studio import APIAccess as api
+
+# Point to the local server
+print(api.getKey())
+client = OpenAI(api_key=api.getKey())
+
+contexts=["Unknown", "Weather", "Greet"]
+prompt="You are an assistant which is requested to find the context of the sentence. Find the closest context with the following keywords "
+prompt+=contexts[0] + ", "
+prompt+=contexts[1] + ", "
+prompt+=contexts[2]
+
+message = [
+    {"role": "system", "content": prompt},
+    {"role": "user", "content": input()}
+]
+
+completion = client.chat.completions.create(
+    messages=message,
+    model=cst.MODEL_DEFAULT,
+    temperature=0)
+
+print(completion.choices[0].message)
