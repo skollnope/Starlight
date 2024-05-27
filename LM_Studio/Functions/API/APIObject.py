c54e5e13a3e2415872ad82ad7d28302c6692be62
skollnope <antonindebonoa@outlook.fr>
2024-05-27 21:30:07 +0200

    Update APIObject.py

--- LM_Studio/Functions/API/APIObject.py
+++ LM_Studio/Functions/API/APIObject.py
@@ -2,35 +2,66 @@ from typing import Any
 import os
 import json
 
+api_default_directory:str = "c:/Starlight/APIs"
+api_extension_file:str = ".txt"
+
+
+if not os.path.exists(api_default_directory):
+    os.makedirs(api_default_directory)
+
 class APIObject():
     _file:str = None
     _content:dict[str, Any] = None
 
     def __init__(self, 
-                 group:list[str], 
-                 name:str, 
-                 api_key:str=None, 
-                 additionnal_params:dict[str, Any]=None):
+                 name:str,
+                 ip:str=""):
 
-        # Create the file path
-        group_path = os.path.join(*group)
-        dir_path = os.path.join("c:/Starlight/APIs", group_path)
-        file_path = os.path.join(dir_path, f"{name}.txt")
+        self._file = os.path.join(api_default_directory, f"{name}{api_extension_file}")
+        if not self.read_file_content():
+            self._content["name"] = name
+            self._content["ip"] = ip
+            self._content["api_key"] = ""
+            self.save_content()
 
-        # Ensure the directory exists
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
+    @property
+    def name(self) -> str:
+        return self._content["name"]
+    
+    @property
+    def ip(self) -> str:
+        return self._content["ip"]
+    
+    @ip.setter
+    def ip(self, ip:str):
+        self._content["ip"] = ip
+        self.save_content()
+    
+    @property
+    def api_key(self) -> str:
+        return self._content["api_key"]
+
+    @api_key.setter
+    def api_key(self, api_key:str):
+        self._content["api_key"] = api_key
+        self.save_content()
+
+    @property
+    def registered(self) -> bool:
+        return self.api_key != ""
         
-        self._file = file_path
-        
-    def _read_file_content(self):
+    def read_file_content(self) -> bool:
         if os.path.exists(self._file) and os.path.getsize(self._file) > 0:
             with open(self._file, 'r') as file:
                 content = file.read()
                 try:
                     self._content = json.loads(content)
+                    return True
                 except json.JSONDecodeError:
-                    print(f"Error decoding JSON from file {self._file}")
+                    return False
+        else:
+            return False
+
 
     def save_content(self):
         with open(self._file, 'w') as file:
