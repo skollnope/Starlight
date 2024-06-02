cbc6701595fc28a202437d364eb86e140603ba7f
skollnope <antonindebonoa@outlook.fr>
2024-06-02 12:58:37 +0200

    improve the helper

--- LM_Studio/Helpers/Helper_Functions.py
+++ LM_Studio/Helpers/Helper_Functions.py
@@ -2,14 +2,9 @@ from typing import Any, Generator, Callable, get_type_hints, get_args
 
 
 # This function MUST be called when the whole generator has been browsed, otherwise, the result isn't the return one
-def get_generator_result(gen: Generator[str, None, str]) -> str:
+def get_generator_result(gen: Generator[Any, Any, str]) -> str:
     try:
         next(gen) # as the generator is done, it will blow up
     except StopIteration as e:
-        return e.value  # Output: the result of the generators
+        return str(e.value)  # Output: the result of the generators
     return None # is not ended, return None
-
-def get_return_type(callback:Callable) -> Any:
-        type_hint = get_type_hints(callback)["arg"]
-        arg_types, return_type = get_args(type_hint)
-        return return_type
