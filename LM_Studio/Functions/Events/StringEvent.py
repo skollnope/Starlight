0eee28fc589a49c5022faa5c5d08071143b86bce
skollnope <antonindebonoa@outlook.fr>
2024-06-02 18:07:44 +0200

    rename

--- LM_Studio/Functions/Events/StringEvent.py
+++ LM_Studio/Functions/Events/StringEvent.py
@@ -2,12 +2,12 @@ class StringEvent:
     def __init__(self):
         self.handlers = []
 
-    def register(self, handler):
+    def link(self, handler):
         self.handlers.append(handler)
 
-    def unregister(self, handler):
+    def unlink(self, handler):
         self.handlers.remove(handler)
 
-    def trigger(self, arg:str):
+    def emit(self, arg:str):
         for handler in self.handlers:
             handler(arg)
