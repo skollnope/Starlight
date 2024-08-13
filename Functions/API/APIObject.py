b245218b70a64ecc8215e8879361f99e1039c670
skollnope <44549132+skollnope@users.noreply.github.com>
2024-08-13 16:40:51 +0200

    added the room key

--- Functions/API/APIObject.py
+++ Functions/API/APIObject.py
@@ -9,18 +9,35 @@ api_extension_file:str = ".txt"
 if not os.path.exists(api_default_directory):
     os.makedirs(api_default_directory)
 
+# APIObjet content:
+#   - Name, the name you want to give to the object (TV, HIFI, other)
+#   - Room, the room the object is located (can be null) !!!!!!!!!!! TODO: add it to the file and store the files less flat
+#   - ip, the ip to reach the object
+#   - api_key = key to identify the requester (eq. to SSH)
+
+# TODO: allow to "serialize" the object to allow the assisant to know them all
+
 class APIObject():
     _file:str = None
     _content:dict[str, Any] = {}
 
     def __init__(self, 
                  name:str,
+                 room:str = "",
                  ip:str=""):
 
-        self._file = os.path.join(api_default_directory, f"{name}{api_extension_file}")
+        # define the file location
+        if room:
+            self.file = os.path.join(api_default_directory, room)
+        else:
+            self.file = api_default_directory
+        self._file = os.path.join(self.file, f"{name}{api_extension_file}")
+            
+        # fill the api object content
         if not self.read_file_content():
             self._content["name"] = name
             self._content["ip"] = ip
+            self._content["room"] = room
             self._content["api_key"] = ""
             self.save_content()
         elif ip != "" and ip != self.ip:
@@ -51,6 +68,11 @@ class APIObject():
     @property
     def registered(self) -> bool:
         return self.api_key != ""
+    
+    # returns a json object as string
+    @property
+    def serialize(self) -> str:
+        return json.dump(self._content)
         
     def read_file_content(self) -> bool:
         if os.path.exists(self._file) and os.path.getsize(self._file) > 0:
