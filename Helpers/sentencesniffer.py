5018dd927cf899767fdde2f24104d8ccb610c09a
skollnope <44549132+skollnope@users.noreply.github.com>
2024-08-13 19:08:18 +0200

    imporve the architecture

--- Helpers/sentencesniffer.py
+++ Helpers/sentencesniffer.py
@@ -11,32 +11,39 @@ class SentenceSniffer(ABC):
     _client:Any=None
     _model:str=None    
     _contexts:Context = Context()
+    _equipments:list[APIObject] = []
     
-    def __init__(self, model:str, contexts:Context):
+    def __init__(self, model:str, contexts:Context, equipments:list[APIObject]):
+        self._equipments = equipments
         self._contexts = contexts
         self._model=model
 
     @abstractmethod
-    def request4contexts(self, sentence:str) -> list[ContextObject]:
+    def __ask__(self, messages, model, temperature=0) -> str:
         pass
 
-    @abstractmethod
+    def request4contexts(self, sentence:str) -> list[ContextObject]:
+        prompt = DEFAULT_CTX_PROMPT + self._contexts.serialize()
+        message = create_message_with_prompt(prompt, sentence)
+
+        answ = self.__ask__(message)
+        return ContextObject.deserialize(answ)
+
     def request4Equipments(self, sentence:str) -> list[APIObject]:
-        pass
+        prompt = "Prompt for the Equipment" + self._contexts.serialize()
+        message = create_message_with_prompt(prompt, sentence)
+
+        answ = self.__ask__(message)
+        return None
 
 class OpenAISniffer(SentenceSniffer):
     def __init__(self, model:str):
         super.__init__(model)
         self._client = openai.OpenAI(api_key=get_openai_key())
 
-    def request4contexts(self, sentence: str) -> list[ContextObject]:
-        prompt = DEFAULT_CTX_PROMPT + self._contexts.serialize()
-        message = create_message_with_prompt(prompt, sentence)
-
-        completion = self._client.chat.completions.create(messages=message,
+    def __ask__(self, messages, temperature=0) -> str:
+        completion = self._client.chat.completions.create(messages=messages,
                                                     model=self._model,
                                                     temperature=0)
-            
-        contexts = ContextObject.deserialize(completion.choices[0].message.content)
-        return contexts
+        return completion.choices[0].message.content
         
