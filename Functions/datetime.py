241b54a4d3d40b19ed1cf060bc84d54cac9fcfbc
skollnope <44549132+skollnope@users.noreply.github.com>
2025-04-27 20:21:33 +0200

    Update datetime.py

--- Functions/datetime.py
+++ Functions/datetime.py
@@ -5,12 +5,12 @@ from Functions.functions import FunctionCaller, FunctionItem
 from Starlight.context import ContextObject
 
 get_local_time_def: dict[str, Any] = {"name": "get_local_time",
-                                      "description": "return a local date an time from an unknown location"}
+                                      "description": "return the current date and the time as iso format.",}
 def get_local_time(args:dict[str, str]=None) -> str:
     return datetime.now().isoformat()
 
 get_timezone_local_time_def: dict[str, Any] = {"name": "get_timezone_local_time",
-                                              "description": "return the date and the time from a specific timezone",
+                                              "description": "return the current date and the time from a specific timezone as iso format.",
                                               "parameters": {
                                                   "type":"object",
                                                   "properties": {
